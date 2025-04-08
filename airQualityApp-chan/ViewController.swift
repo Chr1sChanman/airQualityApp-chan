e90d1266c1cad9ffc2000c5b7c0607349e85ad7a
Chanman <chrishungchan1027@gmail.com>
2025-04-07 18:45:00 -0700

    AQI IOS App Tutorial creation
    Base creation of an AQI IOS application from a Snap Engineering Academy demo

---
+++ airQualityApp-chan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  airQualityApp-chan
+//
+//  Created by Christopher Chan on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ airQualityApp-chanTests/airQualityApp_chanTests.swift
@@ -0,0 +1,17 @@
+//
+//  airQualityApp_chanTests.swift
+//  airQualityApp-chanTests
+//
+//  Created by Christopher Chan on 4/7/25.
+//
+
+import Testing
+@testable import airQualityApp_chan
+
+struct airQualityApp_chanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
